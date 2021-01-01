a8a0561486df25b12ea89067c44aeb245554267e
shinji0320 <shinji.blue.one@gmail.com>
2021-01-01 09:20:26 +0900

    comment-fix

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,13 +3,15 @@ class PostCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    if comment.save
+      redirect_to book_path(book)
+    else
+      render 'books/show'
   end
 
   def destroy
     PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
 
   private
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,5 +1,5 @@
 class PostComment < ApplicationRecord
     belongs_to :user
     belongs_to :book
-    validates :comments, presence: true
+    validates :comment, presence: true
 end
