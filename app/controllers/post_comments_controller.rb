8c223291a83b913f28124a0ce9b10aa2dacf21dd
shinji0320 <shinji.blue.one@gmail.com>
2021-01-06 10:07:35 +0900

    Ajax_comments_fix

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,18 +4,13 @@ class PostCommentsController < ApplicationController
 		@post_comment = PostComment.new(post_comment_params)
 		@post_comment.book_id = @book.id
 		@post_comment.user_id = current_user.id
-		if @post_comment.save
-  		redirect_to book_path(@book.id)
-		else
-      render 'books/show'
-    end
+		@post_comment.save
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-  	post_comment = @book.post_comments.find(params[:id])
-		post_comment.destroy
-    redirect_to request.referer
+  	@post_comment = @book.post_comments.find(params[:id])
+		@post_comment.destroy
   end
 
   private
