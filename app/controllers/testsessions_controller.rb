eb983c9bc23887d248353ccefdf4b67c99d671f5
shinji0320 <shinji.blue.one@gmail.com>
2021-01-12 14:00:57 +0900

    fix

--- app/controllers/testsessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class TestsessionsController < ApplicationController
-    def create
-        user=User.find_by(name:"test1")
-        session[:user_id] = user.id
-        flash[:success] = "テストユーザとしてログインしました。"
-        redirect_to user
-    end
-end
